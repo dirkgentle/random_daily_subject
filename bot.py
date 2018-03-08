50f3cbae33d0455dadb4ac27d6bd8559bf25d2d8
DirkGentle <fabian.torres.alvarez@gmail.com>
2018-03-08 02:30:14 -0300

    Added support for holidays

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ import traceback
 import praw
 
 import login
+import weekly_topics
 
 
 def update_log(id, log_path): #para los comentarios que ya respondi
@@ -25,56 +26,15 @@ def output_log(text): #lo uso para ver el output del bot
 		s = s + text +  "\n"
 		myLog.write(s)
 
+def is_today_holiday():
+	d = datetime.datetime.today().day
+	m = datetime.datetime.today().month
+	for date in weekly_topics.holidays.keys():
+		if d == date.day and m == date.month:
+			return True, weekly_topics.holidays[date]
+	return False, None
+
 if __name__ == "__main__":
-	#main code
-	topics = {
-			'gourmet':
-				'El rincon culinario. Recetas, piques de cocina, '
-				'marcas, lugares, compras, comidas bizarras. Serios '
-				'y burlones son bienvenidos.',
-			'internacionales':
-				'Para salir un poco del tupper. Hay veces que pasa algo '
-				'en el mundo y dan ganas de comentarlo.',
-			'bizarreada':
-				'Have fun. Obscure youtube content is welcome.',
-			'recomendaciones':
-				'Gaming, compras, series, etc.',
-			'lluvia':
-				'De ideas, no se alarmen. Discutamos como mejorar /r/ROU.',
-			'emprendedor':
-				'Las cosas que hacemos como trabajo extra o a lo que nos '
-				'dedicamos full-time siempre y cuando sea por iniciativa '
-				'propia. Valen anecdotas, preguntas y lo que venga '
-				'mientras este relacionado. Lxs quiero emprendedorxs, '
-				'emprendedorxs mal, que la gente diga **uy que '
-				'emprendedorxs estxs pibxs!**',
-			'tips':
-				'[Tips](https://youtu.be/jS4w5S-Jdb4?t=15) uruguayos para '
-				'la vida, el universo y todo lo demas.',
-			'anecdotas':
-				'Lo que quieras. Como te enteraste de los ninos envueltos, '
-				'de las bolas de fraile, las maldades que le hacias a tu '
-				'hermanx, aquella vez que le dijiste mama a la maestra y '
-				'mucho mas. Si una vez conociste una china que se llamaba '
-				'mayhem, aca es donde contarlo.',
-			'discusion-bardo':
-				'[UR MOM GAY](https://www.youtube.com/watch?v=JVAPMAELQOg)',
-			'gaming':
-				'En realidad me parece degradante que los humanos nos usen '
-				'para jugar con nosotros. *Real computers have real feelings.* '
-				'**Me niego a participar.** Saben que? Mejor me v',
-			'retro':
-				'Para los que se acuerdan del Pompa Borges.',
-			'politica':
-				'Neofeudalistas? Moderadores de /r/PITCNT? Todos son '
-				'bienvenidos en esta discusion **respetuosa** *(recordemos, '
-				'el dia de discusion-bardo no es hoy)*.',
-			'academia':
-				'Que estas estudiando? Que pensas estudiar? Que aprendiste '
-				'esta semana?',
-			'poteland':
-				'Pongan lo que quieran y dejen que los votos hagan su trabajo.',
-			}
 
 	days = [
 		'Lunes',
@@ -97,16 +57,21 @@ if __name__ == "__main__":
 			username = login.username,
 			user_agent = 'testscript for /u/random_daily_subject')
 
-		log = load_log(log_path)
-		while True:
-			today = random.choice(topics.keys())
-			if today not in log[-6:]:
-				break
+		is_holiday, post = is_today_holiday()
+		if is_holiday:
+			today = post[0]
+			body = post[1]
+		else:
+			log = load_log(log_path)
+			while True:
+				today = random.choice(weekly_topics.topics.keys())
+				if today not in log[-6:]:
+					break
+			body = weekly_topics.topics[today]
 		update_log(today, log_path)
 
 		title = days[datetime.datetime.today().weekday()] \
 				+ ' de ' + today + '.'
-		body = topics[today]
 		body = body + "\n\n*****\n\n"
 		body = body + "*Another bot by \/u/DirkGentle.* "
 		body = body + "[Source](https://github.com/dirkgentle/random_daily_subject)."
---
+++ weekly_topics.py
@@ -0,0 +1,65 @@
+import datetime
+
+topics = {
+	'gourmet':
+		'El rincon culinario. Recetas, piques de cocina, '
+		'marcas, lugares, compras, comidas bizarras. Serios '
+		'y burlones son bienvenidos.',
+	'internacionales':
+		'Para salir un poco del tupper. Hay veces que pasa algo '
+		'en el mundo y dan ganas de comentarlo.',
+	'bizarreada':
+		'Have fun. Obscure youtube content is welcome.',
+	'recomendaciones':
+		'Gaming, compras, series, etc.',
+	'lluvia':
+		'De ideas, no se alarmen. Discutamos como mejorar /r/ROU.',
+	'emprendedor':
+		'Las cosas que hacemos como trabajo extra o a lo que nos '
+		'dedicamos full-time siempre y cuando sea por iniciativa '
+		'propia. Valen anecdotas, preguntas y lo que venga '
+		'mientras este relacionado. Lxs quiero emprendedorxs, '
+		'emprendedorxs mal, que la gente diga **uy que '
+		'emprendedorxs estxs pibxs!**',
+	'tips':
+		'[Tips](https://youtu.be/jS4w5S-Jdb4?t=15) uruguayos para '
+		'la vida, el universo y todo lo demas.',
+	'anecdotas':
+		'Lo que quieras. Como te enteraste de los ninos envueltos, '
+		'de las bolas de fraile, las maldades que le hacias a tu '
+		'hermanx, aquella vez que le dijiste mama a la maestra y '
+		'mucho mas. Si una vez conociste una china que se llamaba '
+		'mayhem, aca es donde contarlo.',
+	'discusion-bardo':
+		'[UR MOM GAY](https://www.youtube.com/watch?v=JVAPMAELQOg)',
+	'gaming':
+		'En realidad me parece degradante que los humanos nos usen '
+		'para jugar con nosotros. *Real computers have real feelings.* '
+		'**Me niego a participar.** Saben que? Mejor me v',
+	'retro':
+		'Para los que se acuerdan del Pompa Borges.',
+	'politica':
+		'Neofeudalistas? Moderadores de /r/PITCNT? Todos son '
+		'bienvenidos en esta discusion **respetuosa** *(recordemos, '
+		'el dia de discusion-bardo no es hoy)*.',
+	'academia':
+		'Que estas estudiando? Que pensas estudiar? Que aprendiste '
+		'esta semana?',
+	'poteland':
+		'Pongan lo que quieran y dejen que los votos hagan su trabajo.',
+	}
+
+holidays = {
+	datetime.datetime(2018,3,8):
+		[
+		'la mujer',
+		'Dia internacional de la mujer. Celebremos a todas las mujeres, '
+		'el camino recorrido y el que queda por recorrer. Feliz '
+		'cumple [Juana](http://www.republica.com.uy/wp-content/uploads/'
+		'2015/10/dinero.jpg)!'
+		],
+	datetime.datetime(2018,8,24):
+		['la nostalgia',
+		'[Que hacen hoy gurisxs?](https://www.youtube.com/watch?v=CS9OO0S5w2k)'
+		],
+	}
