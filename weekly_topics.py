3ce342cdfc66c9111cd995fb3d4eceb1e1f01d5f
masaxo00 <masaxo00@gmail.com>
2018-03-29 19:20:22 -0300

    Le agregué un día de arte al bot.
    Capaz no funciona porque somos pocos, pero visto que hay varios artistas en el sub estaría bueno tener un día donde puedan mostrar todo lo que hacen.
    
    PD: No entiendo un joraca como funciona el github, creo que te mandé un edit antes que esta todo mal (borraba texto), no le hagas caso. :)

--- weekly_topics.py
+++ weekly_topics.py
@@ -59,6 +59,10 @@ topics = {
         'No hay preguntas boludas. Es tu chance de preguntar [**eso**] '
         '(https://youtu.be/c4lCMa73r_I?t=24) que te está matando hace rato.'
         ' *No te preocupes, yo tampoco sé quien es el Pompa Borges*.',
+     'arte':
+        '¡Dale!, saca todos tus proyectos artisticos a la luz. '
+        'Spamea todo tu arte en este post: '
+        'Musica, arte plastico, chistes, todo lo que sea OC.',
     }
 
 holidays = { # Keys are in (month, day) format
