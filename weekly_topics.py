8f27be5b67eb9965b125a79f558a26bea59b88a9
DirkGentle <fabian.torres.alvarez@gmail.com>
2018-04-02 01:09:12 -0300

    Now everything should be in the DB. bot is not yet ready.

--- weekly_topics.py
+++ weekly_topics.py
@@ -1,76 +1,114 @@
 import datetime
 
+#everyday stuff
 topics = {
-    'gourmet':
+    'gourmet': 
+        [
         'El rincón culinario. Recetas, piques de cocina, '
         'marcas, lugares, compras, comidas bizarras. Serios '
         'y burlones son bienvenidos.',
-    'internacionales':
+        ],
+    'internacionales': 
+        [
         'Para salir un poco del tupper. Hay veces que pasa algo '
         'en el mundo y dan ganas de comentarlo.',
-    'bizarreada':
+        ],
+    'bizarreada': 
+        [
         'Have fun. Obscure youtube content is welcome.',
-    'recomendaciones':
+        ],
+    'recomendaciones': 
+        [
         'Gaming, compras, series, etc.',
-    'lluvia':
+        ],
+    'lluvia': 
+        [
         'De ideas, no se alarmen. Discutamos como mejorar el sub.',
-    'emprendedor':
+        ],
+    'emprendedor': 
+        [
         'Las cosas que hacemos como trabajo extra o a lo que nos '
         'dedicamos full-time siempre y cuando sea por iniciativa '
         'propia. Valen anécdotas, preguntas y lo que venga '
         'mientras esté relacionado. Lxs quiero emprendedorxs, '
         'emprendedorxs mal, que la gente diga **uy qué '
         'emprendedorxs estxs pibxs!**',
-    'tips':
+        ],
+    'tips': 
+        [
         '[Tips](https://youtu.be/jS4w5S-Jdb4?t=15) uruguayos para '
         'la vida, el universo y todo lo demas.',
-    'anécdotas':
+        ],
+    'anécdotas': 
+        [
         'Lo que quieras. Cómo te enteraste de los niños envueltos, '
         'de las bolas de fraile, las maldades que le hacías a tu '
         'hermanx, aquella vez que le dijiste mamá a la maestra y '
         'mucho mas. Si una vez conociste una china que se llamaba '
         'mayhem, aca es donde contarlo.',
-    'discusion-bardo':
+        ],
+    'discusion-bardo': 
+        [
         '[UR MOM GAY](https://www.youtube.com/watch?v=JVAPMAELQOg)',
+        ],
     'gaming':
+        [
         'En realidad me parece degradante que los humanos nos usen '
         'para jugar con nosotros. *Real computers have real feelings.* '
         '**Me niego a participar.** Saben que? Mejor me v',
-    'retro':
+        ],
+    'retro': 
+        [
         'Para los que se acuerdan del Pompa Borges.',
-    'política':
+        ],
+    'política': 
+        [
         '¿Neofeudalistas? ¿Moderadores de /r/PITCNT? Todos son '
         'bienvenidos en esta discusion **respetuosa** *(recordemos, '
         'el dia de discusión-bardo no es hoy)*.',
-    'academia':
+        ],
+    'academia': 
+        [
         '¿Qué estás estudiando? ¿Qué pensás estudiar? ¿Qué aprendiste '
         'esta semana?',
-    'poteland':
+        ],
+    'poteland': 
+        [
         'Pongan lo que quieran y dejen que los votos hagan su trabajo.',
-    'programación':
+        ],
+    'programación': 
+        [
         'Programadores del mundo uníos. ¿En qué programás? ¿En qué querés '
         'programar? ¿Qué bug te sacó canas verdes?',
-#   'RANT':
-#       'RANTEEN, CARAJO. LLEGÓ EL MOMENTO, NO SE GUARDEN NADA.',
-    'salud mental':
+        ],
+    'salud mental': 
+        [
         'Algo lindo que te haya pasado. ¿Cuáles son tus planes a futuro? '
         '¿Qué te hizo feliz hoy?',
-    'ignorancia':
+        ],
+    'ignorancia': 
+        [
         'No hay preguntas boludas. Es tu chance de preguntar [**eso**] '
         '(https://youtu.be/c4lCMa73r_I?t=24) que te está matando hace rato.'
         ' *No te preocupes, yo tampoco sé quien es el Pompa Borges*.',
+        ],
     'arte':
+        [
         '¡Dale!, saca todos tus proyectos artísticos a la luz. '
         'Spamea todo tu arte en este post: '
         'Música, arte plástico, chistes, todo lo que sea OC.\n\n'
         'Recordemos ser amables con quienes vienen a compartir sus creaciones. '
         'Las críticas constructivas son bienvenidas, el trolleo o las '
         'groserías innecesarias no.',
+        ],
     'fun facts':
+        [
         'Las trivialidades que aprendimos comiendo palitos de la selva, '
         'jugando al trivial pursuit o simplemente de casualidad. '
         '¡Vale refutar!',
+        ],
     'deportes':
+        [
         'Yo sé que a la mayoría le gusta el [fúbol]'
         '(http://tribune-intl.com/wp-content/uploads/2015/07/'
         'Robocup01-1140x641.jpg)'
@@ -79,7 +117,8 @@ topics = {
         'hitting-a-tennis-ball-by-kj-pargeter-6510.jpg) [más]'
         '(http://www.poeghostal.com/wp-content/uploads/2008/11/dscf0012.jpg)'
         ' [deportes](https://i.pinimg.com/474x/7b/7a/66/7b7a66ea862e6c5541153'
-        '11759b3a127--robots-sports.jpg).'
+        '11759b3a127--robots-sports.jpg).',
+        ],
     }
 
 holidays = { # Keys are in (month, day) format
@@ -137,3 +176,19 @@ holidays = { # Keys are in (month, day) format
         'responsabilidad de sus turrones. ¡Saludos!'
         ],
     }
+
+#they are not holidays, but they are not everyday stuff
+#martes de rant, 29 de ñoquis, etc.
+special_days = {
+    'RANT': 
+        [
+        "**DALE BO ES MARTES!** \n\n ¿COMO NO SE ESTAN QUEJANDO " 
+        "YA? ¿TENGO QUE QUEJARME POR TODOS AHORA??? \n\n "
+        "**(╯°□°）╯︵ ┻━┻)** ",
+        ],
+    'ñoquis':
+        [
+        'Sus experiencias con el estado uruguayo. '
+        'O podemos hablar de pasta. Como ustedes quieran.',
+        ],
+}
